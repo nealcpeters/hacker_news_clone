befa858d3f2ec6321bf27a0683356a50d9e59a7b
nealcpeters <nealcpeters@gmail.com>
2014-02-26 15:05:43 -0600

    added post editing and added post functionality to index

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -8,7 +8,9 @@ end
 get '/view' do
   post_id = params[:post_id]
   if post_id != nil && post_id != ""
-    @post_data = Post.where('post_id = ?',post_id).first
+    @post_data = Post.where('id = ?',post_id).first
+    @comments = Comment.where('post_id = ?',post_id)
+
     erb :view_post
   end
 end
